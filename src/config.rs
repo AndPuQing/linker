a6e73a04344c14383b85d46ad86edf5f386fe5d0
PuQing <me@puqing.work>
2023-12-21 13:47:55 +0800

    Add configuration functionality for the linker

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,25 @@
+/// This module contains the implementation of the configuration functionality for the linker.
+/// It provides functions for parsing and writing the configuration file, as well as retrieving the default configuration path.
+/// The configuration file is in JSON format and contains information about the resources used by the linker.
 use serde::{Deserialize, Serialize};
 use std::{fs, io::ErrorKind, path::Path};
 
+/// Represents a resource configuration.
 #[derive(Serialize, Deserialize)]
 pub struct ResourceConfig {
     pub(crate) name: String,
     pub(crate) path: String,
 }
 
+/// Represents the overall configuration.
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub(crate) resources: Vec<ResourceConfig>,
 }
 
+/// Parses the configuration file at the specified file path and returns the parsed configuration.
+/// If the file does not exist, a new configuration file will be created with default values.
+/// If the file cannot be read or parsed, an error will be returned.
 pub fn parse_config_file(file_path: &str) -> Config {
     let contents = fs::read_to_string(file_path).unwrap_or_else(|_| {
         log::warn!("Config file not found, creating one");
@@ -26,6 +34,9 @@ pub fn parse_config_file(file_path: &str) -> Config {
     config
 }
 
+/// Writes the configuration to the specified file path.
+/// If the directory or file does not exist, they will be created.
+/// If the file cannot be written, an error will be returned.
 pub fn write_config_file(file_path: &str, config: &Config) {
     let contents = serde_json::to_string_pretty(config).expect("Failed to serialize config");
     fs::write(file_path, contents.clone()).unwrap_or_else(|error| {
@@ -40,10 +51,40 @@ pub fn write_config_file(file_path: &str, config: &Config) {
     });
 }
 
+/// Returns the default configuration file path.
+/// The default path is in the user's home directory under the ".linker" directory.
 pub fn get_default_config_path() -> String {
     let home_dir = dirs::home_dir().expect("Failed to get home directory");
     let config_dir = home_dir.join(".linker");
     let config_file = config_dir.join("config.json");
     log::info!("Config file path: {}", config_file.to_str().unwrap());
     config_file.to_str().unwrap().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn test_parse_config_file() {
+        let _config = parse_config_file("config.json");
+        assert_eq!(_config.resources.len(), 0);
+    }
+
+    #[test]
+    fn test_write_config_file() {
+        let _config = parse_config_file("config.json");
+        let _resource = ResourceConfig {
+            name: "test".to_string(),
+            path: "test".to_string(),
+        };
+        let _config = Config {
+            resources: vec![_resource],
+        };
+        write_config_file("config.json", &_config);
+        let _config = parse_config_file("config.json");
+        assert_eq!(_config.resources.len(), 1);
+        fs::remove_file("config.json").unwrap();
+    }
 }
